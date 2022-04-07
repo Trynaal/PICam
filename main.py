c3b878fcf3c112d822e15b8c0d0173f971583c25
Pitymivo <68125027+Paul2009-38@users.noreply.github.com>
2022-04-07 22:06:25 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+num=0
+videoWebcam = cv2.VideoCapture(0)
+face_model = cv2.CascadeClassifier("fullbody.xml")
+while True:
+
+    valeurRetour, imageWebcam = videoWebcam.read()
+    gray = imageWebcam
+    #gray = cv2.cvtColor(imageWebcam, cv2.COLOR_BGR2GRAY)
+    faces = face_model.detectMultiScale(gray)
+    visage=False
+    for face in faces:
+        cv2.rectangle(gray, (face[0], face[1]), (face[0] + face[2], face[0] + face[3]), (255, 0, 0), 3)
+        out = cv2.imwrite('/var/www/html/uploads/%s.png' % (num), imageWebcam)
+        num=num+1
+
+videoWebcam.release()
+cv2.destroyAllWindows()
